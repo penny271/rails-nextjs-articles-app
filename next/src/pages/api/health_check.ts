80500459d551cbc46291c06b370aec1addf554c8
penny271 <hirotaka.aoki27@gmail.com>
2024-03-22 09:17:26 +0900

    本番環境へのデプロイを見据えた、Next.jsの API Routes 機能を用いたヘルスチェック用APIを実装

---
+++ next/src/pages/api/health_check.ts
@@ -0,0 +1,10 @@
+// next/src/pages/api/health_check.ts
+// API Routes の機能を利用して、本番環境にデプロイした際の疎通確認として使用するヘルスチェック機能を実装
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ status: 'ok' }));
+}
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
       get "health_check", to: "health_check#index"
       # ターミナルでdeviseインストールと一緒に追加された User に対するルーティングを適切な定義（api/v1配下）に直す(移動した)
       mount_devise_token_auth_for "User", at: "auth"
+      # The sign-up route would be /api/v1/auth/sign_up
+      # The sign-in route would be /api/v1/auth/sign_in
+      # The sign-out route would be /api/v1/auth/sign_out
+      # Other related routes for account confirmation, password recovery, etc., would also be nested under /api/v1/auth
       # user情報を更新する - created_atに日時を入れる(サインアップ時)
       namespace :user do
         resource :confirmations, only: [:update]
